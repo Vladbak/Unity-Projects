8cdffd994f8217e8d0212d5dd50cb5e9b5be9b39
Vladimir Bakshenov <vladbaksh@gmail.com>
2018-07-24 16:08:12 +0300

    Jumping ball game project. Already created gamefield, player-ball, adjusted camera to follow player and response to mouse moving. Player can move and jump, bit it has to be regulated

---
+++ Jumping ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+    private GameObject Player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        if (Player == null)
+            throw new Exception("no plyer found");
+
+        offset = new Vector3(0, 20, -20);
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        transform.position = Player.transform.position + offset;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+}
---
+++ Jumping ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public int Speed;
+    public float PowerofJump;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Speed = 15;
+        PowerofJump = 300.0f;
+	    rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0.0f, PowerofJump ,0.0f);
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+
+    }
+}
